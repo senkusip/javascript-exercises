854a3944aa182744e2a0cd4074802ab9cdd334d3
senkusip <163171435+senkusip@users.noreply.github.com>
2025-03-12 12:09:36 -0600

    Complete excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    const reversedStringArray = [];
+        for (let i = stringArray.length; i >= 0; i--){
+            reversedStringArray.push(stringArray[i]);
+        }
+    let reversedString = reversedStringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
